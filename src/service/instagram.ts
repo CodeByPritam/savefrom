d4fdca56607c9d4f5815773892818f3cfaa41bd5
CodeByPritam <iampritamadak@gmail.com>
2026-04-09 19:25:20 +0530

    added service and utils layer

---
+++ src/service/instagram.ts
@@ -0,0 +1,3 @@
+import { fetch, ProxyAgent } from 'undici';
+import _Config from '../config/config.js';
+import { URLSearchParams } from "node:url";
---
+++ src/utils/igUtils.ts
@@ -0,0 +1,40 @@
+const dissectIgUrl = (url: string) => {
+
+    // Normalize
+    let nrml = (url as string)
+    .replace(/^https?:\/\//, '')
+    .replace(/^www\./, '')
+    .replace(/^instagram\.com\//, '')
+    .replace(/\/$/, '');
+
+    // Pattern Check
+    const patterns = {
+        postRegex: /^p\/([a-zA-Z0-9_\-]+)\/?$/,
+        reelsRegex: /^reels?\/([a-zA-Z0-9_\-]+)\/?$/,
+        audioRegex: /^reels?\/audio\/(\d+)\/?$/,
+        BigUrlFormatHighlightsRegex: /^stories\/highlights\/(\d+)\/?$/,
+        shortUrlFormatHighlightRegex: /^s\/([a-zA-Z0-9]+)\/?$/,
+        storiesRegex: /^stories\/([a-zA-Z0-9_.\-]+)(?:\/(\d+))?\/?$/,
+        profileRegex: /^([a-zA-Z0-9_.\-]+)\/?$/,
+    }
+
+    // Matching
+    for (const [type, pattern] of Object.entries(patterns)) {
+        const match = nrml.match(pattern);
+        if (match) {
+            return {
+                shortcode: match[1],
+                isPost: (type === 'postRegex'),
+                isReel: (type === 'reelsRegex'),
+                isAudio: (type === 'audioRegex'),
+                isProfile: (type === 'profileRegex'),
+                isStory: (type === 'storiesRegex'),
+                isHighlight: (type === 'bigUrlFormatHighlightsRegex' || type === 'shortUrlFormatHighlightRegex'),
+            }
+        }
+    }
+
+};
+
+// Export
+export { dissectIgUrl };
